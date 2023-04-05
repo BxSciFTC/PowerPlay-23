52aec22fd514b327a922ce44f2607d1eea61e7bb
BluePixelPython <67238525+BluePixelPython@users.noreply.github.com>
2023-04-04 20:46:00 -0400

    ALL hardware classes inherit Mechanism

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardWare/MechanismInherit.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.HardWare;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+
+/*
+All mechanisms of the robot should extend this class
+    That is, hardware classes which are in the "hardware" folder
+It ensures that all such classes use the Hardware Map
+ */
+
+public abstract class MechanismInherit {
+
+    /*
+    Initializes all the robot's hardware
+    Gets and stores references to the robot configuration
+        As in, gets and stores the names of each part, and where they are connected on the control/expansion hub
+     */
+    public abstract void init(HardwareMap hwMap);
+}
