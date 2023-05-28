9429e9aeebfd3e78719d12d80758d5546e2f4b4e
Bryan Liu <liub3@bxscience.edu>
2023-05-28 01:02:28 -0400

    Autonomous and OpenCV finished. Robot completed. :D

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/autonomous/FiveConeLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/autonomous/FiveConeLeft.java
@@ -1,4 +1,10 @@
 package org.firstinspires.ftc.teamcode.opMode.autonomous;
 
 public class FiveConeLeft {
+    /*
+    *
+    * You get the idea.
+    * Copy FiveConeRight into here, but change the constants.
+    *
+    * */
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/autonomous/FiveConeRight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opMode/autonomous/FiveConeRight.java
@@ -1,14 +1,25 @@
 package org.firstinspires.ftc.teamcode.opMode.autonomous;
 
+import android.annotation.SuppressLint;
+
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.hardware.IntakeLiftFSM;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.firstinspires.ftc.teamcode.util.AprilTagDetectionPipeline;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+import java.util.ArrayList;
 
 
 /*
@@ -30,11 +41,28 @@ public class FiveConeRight extends LinearOpMode {
     public SampleMecanumDrive drive;
     public IntakeLiftFSM arm = new IntakeLiftFSM();
     ElapsedTime timer = new ElapsedTime();
+    OpenCvWebcam cam;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+    AprilTagDetection tagOfInterest = null;
+
+    double fx = 578.272;
+    double fy = 578.272;
+    double cx = 402.145;
+    double cy = 221.506;
+
+    // UNITS ARE METERS
+    double tagsize = 0.166;
+
+    //Tag IDs (Sleeve)
+    final int left = 17;
+    final int middle = 18;
+    final int right = 19;
 
     public static double servoTime = 0.625;
     public static double liftTime = 0.625;
 
     Pose2d endPose;
+    public static int coneCount = 0;
 
     @Override
     public void runOpMode() {
@@ -62,6 +90,7 @@ public class FiveConeRight extends LinearOpMode {
                 .lineToLinearHeading(AutoConstants.R_SCORE_POST)
                 .UNSTABLE_addTemporalMarkerOffset(0, () -> {
                     arm.score();
+                    coneCount++;
                 })
                 .waitSeconds(servoTime)
                 .UNSTABLE_addTemporalMarkerOffset(0, () -> {
@@ -85,6 +114,7 @@ public class FiveConeRight extends LinearOpMode {
                 .lineToLinearHeading(AutoConstants.R_SCORE_POST)
                 .UNSTABLE_addTemporalMarkerOffset(0, () -> {
                     arm.score();
+                    coneCount++;
                 })
                 .waitSeconds(servoTime)
                 .UNSTABLE_addTemporalMarkerOffset(0, () -> {
@@ -114,7 +144,89 @@ public class FiveConeRight extends LinearOpMode {
                 .build();
 
         //OPENCV
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        cam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "camera"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+        cam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(
+                WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        cam.setPipeline(aprilTagDetectionPipeline);
 
+        cam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                cam.startStreaming(800, 600, OpenCvCameraRotation.UPRIGHT);
+            }
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("Camera failed :((", "D:");
+                telemetry.update();
+            }
+        });
+
+        telemetry.setMsTransmissionInterval(50);
+        /*
+         * The INIT-loop:
+         * This REPLACES waitForStart!
+         */
+        while (!isStarted() && !isStopRequested())
+        {
+            ArrayList<AprilTagDetection> currentDetections = aprilTagDetectionPipeline.getLatestDetections();
+
+            if(currentDetections.size() != 0)
+            {
+                boolean tagFound = false;
+
+                for(AprilTagDetection tag : currentDetections)
+                {
+                    if(tag.id == left || tag.id == middle || tag.id == right)
+                    {
+                        tagOfInterest = tag;
+                        tagFound = true;
+                        break;
+                    }
+                }
+
+                if(tagFound)
+                {
+                    telemetry.addLine("Tag of interest is in sight!\n\nLocation data:");
+                    tagToTelemetry(tagOfInterest);
+                }
+                else
+                {
+                    telemetry.addLine("Don't see tag of interest :(");
+
+                    if(tagOfInterest == null)
+                    {
+                        telemetry.addLine("(The tag has never been seen)");
+                    }
+                    else
+                    {
+                        telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                        tagToTelemetry(tagOfInterest);
+                    }
+                }
+
+            }
+            else
+            {
+                telemetry.addLine("Don't see tag of interest :(");
+
+                if(tagOfInterest == null)
+                {
+                    telemetry.addLine("(The tag has never been seen)");
+                }
+                else
+                {
+                    telemetry.addLine("\nBut we HAVE seen the tag before; last seen at:");
+                    tagToTelemetry(tagOfInterest);
+                }
+
+            }
+
+            telemetry.update();
+            sleep(20);
+        }
+        cam.stopStreaming();
 
         drive.followTrajectorySequenceAsync(initial);
         endPose = initial.end();
@@ -123,7 +235,30 @@ public class FiveConeRight extends LinearOpMode {
             endPose = mainPath.end();
         }
 
+        /*
+         * The INIT-loop:
+         * This REPLACES waitForStart!
+         */
+        timer.reset();
+        while (!isStarted() && !isStopRequested()) {
+            drive.update();
+            arm.loop();
+            if (coneCount == 5) {
+                if (tagOfInterest.id == left) {
+                    drive.followTrajectorySequenceAsync(leftPark);
+                }else if (tagOfInterest.id == middle) {
+                    drive.followTrajectorySequenceAsync(midPark);
+                }else if (tagOfInterest.id == right) {
+                    drive.followTrajectorySequenceAsync(rightPark);
+                }
+            }
+            telemetry.update();
+        }
         //follow to parking
     }
 
+    @SuppressLint("DefaultLocale")
+    void tagToTelemetry(AprilTagDetection detection) {
+        telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+    }
 }
